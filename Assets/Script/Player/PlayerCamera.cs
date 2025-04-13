f808c4dc40d7c7de8995c85f7674bb4d392cec8d
Walid <wldelmouch@gmail.com>
2025-04-13 23:44:45 +0200

    legroszgegaAmin

--- Assets/Script/Player/PlayerCamera.cs
+++ Assets/Script/Player/PlayerCamera.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerCamera : MonoBehaviour
 {
     [Header("Références")]
     [Tooltip("Référence à la caméra du joueur")]
